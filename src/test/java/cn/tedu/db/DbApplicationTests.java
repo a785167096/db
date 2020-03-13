9bb7e695286b53214154bd34e4ae009c1988c9e5
a785167096 <ljf.320@foxmail.com>
2020-03-14 00:07:49 +0800

    000

--- src/test/java/cn/tedu/db/DbApplicationTests.java
+++ src/test/java/cn/tedu/db/DbApplicationTests.java
@@ -1,31 +1,34 @@
 package cn.tedu.db;
 
-import org.junit.jupiter.api.Test;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import javax.sql.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DbApplicationTests {
+	
+	@Autowired
+	DataSource ds;
+	
+	@Test
+	public void getConnection() throws SQLException {
+		Connection conn=ds.getConnection();
+		System.err.println(conn);
+	}
 
-    @Autowired
-    DataSource ds;
-
-    @Test
-    public void getConnection() throws SQLException {
-        Connection conn = ds.getConnection();
-        System.err.println(conn);
-    }
-
-
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
+	
+	
 
 }
